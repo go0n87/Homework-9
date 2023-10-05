948088c495999d47b3d6287725b5f4855152f390
go0n87 <98361866+go0n87@users.noreply.github.com>
2023-10-05 15:44:01 +0300

    15:43 05.10.2023

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    private Scene _currentScene;
+
+    int _indexCurrentScene;
+
+    private void Start()
+    {
+        _indexCurrentScene =    _getCurrentBuildIndex();
+    }
+
+    public void Superman()
+    {
+        _indexCurrentScene  =   _getCurrentBuildIndex();
+        SceneManager.LoadScene(0);
+        SceneManager.UnloadSceneAsync(_indexCurrentScene);
+     
+    }
+
+    public void Billiard()
+    {
+        _indexCurrentScene = _getCurrentBuildIndex();
+        SceneManager.LoadScene(1);
+        SceneManager.UnloadSceneAsync(_indexCurrentScene);
+      
+    }
+
+
+    private int _getCurrentBuildIndex()
+    {
+        return SceneManager.GetActiveScene().buildIndex;
+    }
+}
